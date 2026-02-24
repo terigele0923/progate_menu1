720dc330b4eba47000dbebe3ac44cf5a196c5fdd
devterigele <dev.terigele@gmail.com>
2026-02-24 15:54:16 +0900

    progate phpオブジェクト指向初級

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php require_once 'data.php' ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<title> Cafe Progate </title>
+		<link rel ="stylesheet" type="text/css" href="stylesheet.css">
+		<link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<div class="menu-wrapper container">
+			<h1 class="logo">Cafe Progate </h1>
+			<form action="confirm.php" method="post">
+				<div class ="menu-items">
+					<?php foreach($menus as $menu): ?>
+					<div class="menu-item">
+						<img src="<?php echo $menu->getImage() ?>">
+						<h3 class="menu-item-name"><?php echo $menu->getName() ?></h3>
+						<p class="price"><?php echo $menu->getTaxIncludedPrice() ?>(税込み)</p>
+						<input type="text" vlue="0" name="<?php echo $menu->getName() ?>">
+						<span>個</span>
+					</div>	
+					<?php endforeach ?>
+				</div>
+				<input type="submit" value="注文する">
+			</form>
+		</div>
+	</body>
+</html>
