f2d6817f7ba406d50590950075c91ff05dd9371e
terigele <21010408@anabuki-college.ac.jp>
2026-02-25 16:54:56 +0900

    progate php オブジェクト指向上級完了

--- review.php
+++ review.php
@@ -2,14 +2,23 @@
     class Review {
         private $menuName;
         private $body;
+        private $userId;
 
-        public function __construct($menuName,$body){
+        public function __construct($menuName,$userId,$body){
             $this->menuName = $menuName;
+            $this->userId = $userId;
             $this->body = $body;
         }
 
         public function getMenuName(){
             return $this->menuName;
+        }
+        public function getUser($users){
+            foreach($users as $user){
+                if($user->getId() === $this->userId){
+                    return $user;
+                }
+            }
         }
         public function getBody(){
             return $this->body;
