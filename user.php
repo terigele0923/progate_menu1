496a287aaf11a6073140aa1f99dabf2b00a40285
terigele <21010408@anabuki-college.ac.jp>
2026-02-25 12:45:31 +0900

    user.php追加

---
+++ user.php
@@ -0,0 +1,18 @@
+<?php
+    class User {
+        private $name;
+        private $gender;
+
+        public function __construct($name,$gender){
+            $this->name = $name;
+            $this->gender = $gender;
+        }
+
+        public function getName(){
+            return $this->name;
+        }
+        public function getGender(){
+            return $this->gender;
+        }
+    }
+?>
